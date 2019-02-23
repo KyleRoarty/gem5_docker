96adb3fba90635984624799a5db07a10c0ef9133
Kyle Roarty <kyleroarty1716@gmail.com>
2019-02-23 14:39:27 -0600

    Still messing with docker hub
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -10,6 +10,7 @@ if [ $ret -ne 0 ]; then
 fi
 
 # Test that square works in gem5.
+cd /gem5
 build/GCN3_X86/gem5.opt configs/example/apu_se.py -n2 --benchmark-root=/tests -csquare.out
 ret=$?
 if [ $ret -ne 0 ]; then
