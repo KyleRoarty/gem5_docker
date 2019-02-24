51128e1e99fc25f1a71aaa104686532086b0a810
Kyle Roarty <kyleroarty1716@gmail.com>
2019-02-23 19:35:24 -0600

    Revert previous 3 commits, decided to just turn off autotests in docker hub

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,8 +9,16 @@ if [ $ret -ne 0 ]; then
     exit 1
 fi
 
-# Test that square works in gem5.
 cd /gem5
+
+# Validate that gem5 can be built
+scons -j$(nproc) build/GCN3_X86/gem5.opt --ignore-style
+ret=$?
+if [ $ret -ne 0 ]; then
+    exit 1
+fi
+
+# Test that square works in gem5.
 build/GCN3_X86/gem5.opt configs/example/apu_se.py -n2 --benchmark-root=/tests -csquare.out
 ret=$?
 if [ $ret -ne 0 ]; then
